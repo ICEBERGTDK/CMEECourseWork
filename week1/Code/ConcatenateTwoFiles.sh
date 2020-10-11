cb559c5ddb10913e3cd07d67572e2f92857a9c6e
ICEBERGTDK <dengkui.tang20@imperial.ac.uk>
2020-10-11 14:17:29 +0100

    update shell

--- week1/Code/ConcatenateTwoFiles.sh
+++ week1/Code/ConcatenateTwoFiles.sh
@@ -1,6 +1,28 @@
 #!bin/bash
+if [ -z "$1" -o  -z "$2"  -o ! -e "$1" -o ! -e "$2" ]
+then
+	echo "Please enter corrct filenames"
+elif [ ! -s $1 ]
+then
+	echo "File1 is empty."
+	cat $2 > $3.txt
+	echo "Merged File is "
+	cat $3.txt
+elif [ ! -s $2 ]
+then
+	echo "File2 is empty."
+	cat $1 > $3.txt
+	echo "Merged File is "
+	cat $3.txt
+else
+	cat $1 > $3.txt
+	cat $2 >> $3.txt
+	echo "Merged File is "
+	cat $3.txt
 
-cat $1 > $3
-cat $2 >> $3
-echo "Merged File is "
-cat $3
+fi
+#cat $1 > $3.txt
+#cat $2 >> $3.txt
+#echo "Merged File is "
+#cat $3.txt
+exit
--- week1/Code/tabtocsv.sh
+++ week1/Code/tabtocsv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Author: dengkui.tang20@imperial.ac.uk
 # Script: tabtocsv.sh
 # Description: substitute the tabs in the files with commas
@@ -9,11 +9,29 @@
 
 echo "Creating a comma delimited version of $1 ..."
 #while  "$1"-z"Test.txt"
-	if [[ -z "$1" ]]
+if [ -z "$1" ]
+then
+	echo "Please enter a correct filename $1"
+elif [ -f $1.csv ]
+then 
+	echo "The csv file is already exist. Do you want to replace it?" $ans
+	read ans
+	if [ $ans == "y" ]
 	then
-		echo "Please enter a correct filename $1"
-	else
-		cat $1 | tr -s "\t" "," >> $1.csv
+		cat $1 | tr -s "\t" "," > $1.csv
 		echo "Done!"
+	else
+		echo "ok."
 	fi
+elif [[ $1 == *.csv ]]
+then
+	echo "The file is already a csv file."
+elif [ ! -s $1 ]
+then
+	echo "The file is empty."
+else
+	cat $1 | tr -s "\t" "," >> $1.csv
+	echo "Done!"
+fi
+
 exit
