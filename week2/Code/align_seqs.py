66187acab9242a526486205e03e044ef4a159aab
ICEBERGTDK <dengkui.tang20@imperial.ac.uk>
2020-10-15 11:02:37 +0100

    updated align_seqs

--- week2/Code/align_seqs.py
+++ week2/Code/align_seqs.py
@@ -1,20 +1,29 @@
-#import ipdb; ipdb.set_trace()
+import sys
+
 # Two example sequences to match
-seq2 = "ATCGCCGGATTACGGG"
-seq1 = "CAATTCGGAT"
+# seq2 = "ATCGCCGGATTACGGG"
+# seq1 = "CAATTCGGAT"
+
+def ReadSeq(ListSeq):
+    f = open('../Data/align_seqs.csv','r')
+    for line in f:
+        ListSeq.append(line)
+    return ListSeq
 
 # Assign the longer sequence s1, and the shorter to s2
 # l1 is length of the longest, l2 that of the shortest
 
-l1 = len(seq1)
-l2 = len(seq2)
-if l1 >= l2:
-    s1 = seq1
-    s2 = seq2
-else:
-    s1 = seq2
-    s2 = seq1
-    l1, l2 = l2, l1 # swap the two lengths
+def SwapSeq(seq1,seq2):
+    l1 = len(seq1)
+    l2 = len(seq2)
+    if l1 >= l2:
+        s1 = seq1
+        s2 = seq2
+    else:
+        s1 = seq2
+        s2 = seq1
+        l1, l2 = l2, l1 # swap the two lengths
+    return s1,s2,l1,l2
 
 # A function that computes a score by returning the number of matches starting
 # from arbitrary startpoint (chosen by user)
@@ -30,7 +39,7 @@ def calculate_score(s1, s2, l1, l2, startpoint):
                 matched = matched + "-"
 
     # some formatted output
-    print("." * startpoint + matched)           
+    print("." * startpoint + matched)
     print("." * startpoint + s2)
     print(s1)
     print(score) 
@@ -44,14 +53,31 @@ def calculate_score(s1, s2, l1, l2, startpoint):
 # calculate_score(s1, s2, l1, l2, 5)
 
 # now try to find the best match (highest score) for the two sequences
-my_best_align = None
-my_best_score = -1
 
-for i in range(l1): # Note that you just take the last alignment with the highest score
-    z = calculate_score(s1, s2, l1, l2, i)
-    if z > my_best_score:
-        my_best_align = "." * i + s2 # think about what this is doing!
-        my_best_score = z 
-print(my_best_align)
-print(s1)
-print("Best score:", my_best_score)
+def GetScore(s1,s2,l1,l2):
+    my_best_align = None
+    my_best_score = -1
+    for i in range(l1): # Note that you just take the last alignment with the highest score
+        z = calculate_score(s1, s2, l1, l2, i)
+        if z > my_best_score:
+            my_best_align = "." * i + s2 # think about what this is doing!
+            my_best_score = z 
+    print(my_best_align)
+    print(s1)
+    print("Best score:", my_best_score)
+
+
+def main(argv):
+    ListSeq = []
+    ListSeq = ReadSeq(ListSeq)
+    s1,s2,l1,l2 = SwapSeq(ListSeq[0],ListSeq[1])
+    GetScore(s1,s2,l1,l2)
+    return 0
+
+if (__name__ == "__main__"):
+    status = main(sys.argv)
+    sys.exit(status)
+
+
+
+#create_csv()
