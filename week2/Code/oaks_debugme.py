58d9109b68f8c550bc2f4b0d09ee0ce36f5b0f0e
ICEBERGTDK <dengkui.tang20@imperial.ac.uk>
2021-01-21 15:49:32 +0000

    add groupwork

--- week2/Code/oaks_debugme.py
+++ week2/Code/oaks_debugme.py
@@ -1,60 +1,61 @@
-#!/usr/bin/env python3
+#!/urs/bin/env python3
 
-"""oaks_debugme Practical 2"""
-__author__ = 'Dengkui Tang (dengkui.tang20@imperial.ac.uk)'
+"""Playing with doc tests and debugging"""
+
+__author__ = 'Luke Swaby (lds20@ic.ac.uk), ' \
+             'Jinkai Sun (jingkai.sun20@imperial.ac.uk), ' \
+             'Acacia Tang (t.tang20@imperial.ac.uk), ' \
+             'Dengku Tang (dengkui.tang20@imperial.ac.uk)'
 __version__ = '0.0.1'
 
+## Imports ##
 import csv
 import sys
-import doctest
 
 #Define function
 def is_an_oak(name):
-    """ Returns True if name is starts with 'quercus' 
-    
-    >>> is_an_oak('Quercus')
+    """ Returns True if lowercase name contains 'quercus'
+
+    >>> is_an_oak('Fagus sylvatica')
+    False
+
+    >>> is_an_oak('Fraxinus excelsior')
+    False
+
+    >>> is_an_oak('Quercus ajoensis')
     True
 
-    >>> is_an_oak('Quercs')
-    False
+    >>> is_an_oak('Quercuss ajoensis')
+    True
 
-    >>> is_an_oak('Quercus+s')
-    False
+    >>> is_an_oak('aQuercus')
+    True
 
-    >>> is_an_oak('quercus')
+    >>> is_an_oak('Fagus quercusi')
     False
-
-    >>> is_an_oak('Fraxinus')
-    False
-
     """
+    return 'quercus' in name.split()[0].lower()
 
-    return name=='Quercus' # missed a letter
-"""is an oak"""
-
-def main(argv): 
-    f = open('../Data/TestOaksData.csv','r')
-    g = open('../Data/JustOaksData.csv','w')
+def main():
+    """Run functions
+    """
+    f = open('../Data/TestOaksData.csv', 'r')
+    g = open('../Data/JustOaksData.csv', 'w')
     taxa = csv.reader(f)
+    header = next(taxa, None)
     csvwrite = csv.writer(g)
-    oaks = set()
+    csvwrite.writerow(header)
+    #oaks = set()
     for row in taxa:
-        if row[0].lower().startswith('genus'): # skip the first line
-            print("This is the title \n")
-            csvwrite.writerow(row)
-        else:
-            print(row)
-            print ("The genus is: ") 
-            print(row[0] + '\n')
-            if is_an_oak(row[0]):
-                print('FOUND AN OAK!\n')
-                csvwrite.writerow([row[0], row[1]])
-                oaks.add(row[1]) # add the names into the 'oaks' set
-    print('Quercus : ',oaks)
-    return 0
-"""the main function"""
-    
-if (__name__ == "__main__"):
-    status = main(sys.argv)
+        print(row)
+        print("The genus is: ")
+        print(row[0] + '\n')
+        if is_an_oak(row[0]):
+            print('FOUND AN OAK!\n')
+            csvwrite.writerow([row[0], row[1]])
 
-doctest.testmod()
+    return 0
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
--- week7/code/run_LV.py
+++ week7/code/run_LV.py
@@ -1,33 +1,67 @@
-import os
-import time 
-import timeit
-import cProfile
+#!/usr/bin/env python3
+
+"""Print profiles on Lotka-Volterra model scripts, taking a single numerical
+argument specifying the number of lines of each profile output the user wants
+printed to the command line"""
+
+## Variables ##
+
+__author__ = 'Luke Swaby (lds20@ic.ac.uk), ' \
+             'Jinkai Sun (jingkai.sun20@imperial.ac.uk), ' \
+             'Acacia Tang (t.tang20@imperial.ac.uk), ' \
+             'Dengku Tang (dengkui.tang20@imperial.ac.uk)'
+__version__ = '0.0.1'
+
+## Imports ##
+
+import sys
+import io
 import pstats
-from LV1 import main as mainlv1
-from LV2 import main as mainlv2
+import cProfile
+import LV1, LV2, LV3, LV4
 
-pr = cProfile.Profile()
-'''LV1.py profiling'''
-pr.enable()
-mainlv1([])
-pr.disable()
-ps = pstats.Stats(pr)
-ps.sort_stats('cumulative').print_stats(10)   
+## Functions ##
 
-pr.enable()
-'''LV2.py profiling'''
-mainlv2([])
-pr.disable()
-ps = pstats.Stats(pr)
-ps.sort_stats('cumulative').print_stats(20)   
+def main(h=0):
+    """Print profiles on Lotka-Volterra model scripts
 
+    Arguments:
+     - h: numerical argument specifying the number of lines of each profile
+          output the user wants printed to the command line.
 
-# start1 = time.time()
-# os.system("python -m cProfile LV1.py")
-# end1 = time.time()
-# start2 = time.time()
-# os.system("python -m cProfile LV2.py")
-# end2 = time.time()
-# print("LV1 Running time: %s Second"%(end1-start1))
-# print("LV2 Running time: %s Second"%(end2-start2))
+    Output:
+     - Printed profiles to the command line.
+    """
+    ## Print profile for each file ##
+    for file in [LV1, LV2, LV3, LV4]:
+        pr = cProfile.Profile()  # allows profile formatting with no file out
+        pr.enable()  # open profile
+        file.main()  # run script
+        pr.disable()  # close profile
 
+        s = io.StringIO()  # create text stream
+
+        # read profile results from file
+        stats = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+
+        # remove extraneous paths from all the module names and only print head
+        stats.strip_dirs().print_stats(h)
+
+        print(f"Profiling '{file.__name__}':")
+        print(s.getvalue())
+
+    return 0
+
+if __name__ == '__main__':
+    # Check if optional profile output number of rows argument is provided
+    if len(sys.argv) == 2:
+        head = int(sys.argv[1])
+        status = main(head)
+    elif len(sys.argv) < 2:
+        print('WARNING: no arguments parsed. Default used = 0.\n')
+        status = main()
+    else:
+        print('WARNING: too many arguments parsed. Default used = 0.\n')
+        status = main()
+
+    sys.exit(status)
